ecc762f3f67cb682cebec59d2f906ae50cf71d00
Jesus Perez <jesusmpds18@gmail.com>
2021-04-21 20:53:07 -0300

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.scss';
+// React Router Dom
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 // Components
 import AppContainer from "./components/AppContainer"
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/ItemListContainer/ItemDetailContainer/ItemDetailContainer"
 
 function App() {
   return (
-    <AppContainer>
-        <NavBar/>
-        <ItemListContainer title ="Tienda" firstSection="Tops"/>
-    </AppContainer>
+    <Router>
+      <AppContainer>
+          <NavBar/>
+          <Switch>
+            <Route exact path="/">
+                <ItemListContainer title ="Tienda" firstSection="Tops" secondSection="Calzas"/>
+            </Route>
+            <Route exact component={ItemDetailContainer} path="/product/:id" />
+            <Route path="/category/:categoryName">
+              <ItemListContainer title ="Tienda" firstSection="Tops" secondSection="Calzas"/>
+            </Route>
+          </Switch>
+      </AppContainer>
+    </Router>
   );
 }
 
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,27 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import ItemList from "./ItemList/ItemList"
-import ItemDetailContainer from "./ItemDetailContainer/ItemDetailContainer"
 import '../../App.scss'
+import { useParams } from 'react-router'
 
-const ItemListContainer = ({title,firstSection}) =>{
-    const [productList, setProductList]= useState([]);
+const ItemListContainer = ({title,firstSection,secondSection}) =>{
+    const [productListTops, setProductListTops]= useState([]);
+    const [productListCalzas, setProductListCalzas]= useState([]);
+    const {categoryName} = useParams();
 
     useEffect(()=>{
       fetch('productos.json')
         .then((response) => response.json())
-        .then((myJson) => setProductList(myJson.tops))
+        .then((myJson) => {
+            console.log(myJson)
+            if (categoryName === undefined){
+            setProductListTops(myJson.tops)
+            setProductListCalzas(myJson.calzas)
+            } else if(categoryName === "tops") {
+                setProductListTops(myJson.tops)
+            } else if (categoryName === "calzas"){
+                setProductListCalzas(myJson.calzas)
+            }
+        }
+        )
         .catch((e) => console.log(e))
-    },[])
+    },[categoryName])
+
     return (
         <div>
             <h1>{title}</h1>
+            { productListTops && 
             <section className="container">
                 <h2>{firstSection}</h2>
-                <ItemList products={productList}/>
-            </section>
+                <ItemList products={productListTops}/>
+            </section> 
+            }
+            {productListCalzas &&
             <section className="container">
-                <ItemDetailContainer/>
+                <h2>{secondSection}</h2>
+                <ItemList products={productListCalzas}/>
             </section>
+            }
         </div>
     )
 }
